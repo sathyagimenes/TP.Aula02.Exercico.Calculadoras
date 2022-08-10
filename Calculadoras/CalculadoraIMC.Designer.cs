a9117886c3d0287762423f2baca9ff8b0da2d72a
sathya.gimenes <sathya.gimenes@gmail.com>
2022-08-10 19:03:36 -0300

    Updating design of Calculadora IMC

--- Calculadoras/CalculadoraIMC.Designer.cs
+++ Calculadoras/CalculadoraIMC.Designer.cs
@@ -43,51 +43,57 @@
             // 
             // btnLimpar
             // 
-            this.btnLimpar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnLimpar.BackColor = System.Drawing.Color.IndianRed;
+            this.btnLimpar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLimpar.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.btnLimpar.Location = new System.Drawing.Point(319, 326);
             this.btnLimpar.Name = "btnLimpar";
             this.btnLimpar.Size = new System.Drawing.Size(86, 32);
             this.btnLimpar.TabIndex = 4;
             this.btnLimpar.Text = "Limpar\r\n";
-            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.UseVisualStyleBackColor = false;
             this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
             // 
             // btnCalcular
             // 
-            this.btnCalcular.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCalcular.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnCalcular.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCalcular.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.btnCalcular.Location = new System.Drawing.Point(162, 326);
             this.btnCalcular.Name = "btnCalcular";
             this.btnCalcular.Size = new System.Drawing.Size(88, 32);
             this.btnCalcular.TabIndex = 3;
             this.btnCalcular.Text = "Calcular";
-            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.UseVisualStyleBackColor = false;
             this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
             // 
             // lblPeso
             // 
             this.lblPeso.AutoSize = true;
-            this.lblPeso.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblPeso.Location = new System.Drawing.Point(201, 235);
+            this.lblPeso.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPeso.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblPeso.Location = new System.Drawing.Point(187, 234);
             this.lblPeso.Name = "lblPeso";
-            this.lblPeso.Size = new System.Drawing.Size(46, 17);
+            this.lblPeso.Size = new System.Drawing.Size(80, 18);
             this.lblPeso.TabIndex = 4;
-            this.lblPeso.Text = "Peso:";
+            this.lblPeso.Text = "Peso (kg):";
             // 
             // lblAltura
             // 
             this.lblAltura.AutoSize = true;
-            this.lblAltura.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblAltura.Location = new System.Drawing.Point(201, 270);
+            this.lblAltura.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblAltura.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblAltura.Location = new System.Drawing.Point(187, 270);
             this.lblAltura.Name = "lblAltura";
-            this.lblAltura.Size = new System.Drawing.Size(49, 17);
+            this.lblAltura.Size = new System.Drawing.Size(87, 18);
             this.lblAltura.TabIndex = 5;
-            this.lblAltura.Text = "Altura:";
+            this.lblAltura.Text = "Altura (cm):";
             // 
             // lblSistemaCalculo
             // 
             this.lblSistemaCalculo.AutoSize = true;
             this.lblSistemaCalculo.Font = new System.Drawing.Font("Arial", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lblSistemaCalculo.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblSistemaCalculo.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.lblSistemaCalculo.Location = new System.Drawing.Point(122, 171);
             this.lblSistemaCalculo.Name = "lblSistemaCalculo";
             this.lblSistemaCalculo.Size = new System.Drawing.Size(385, 34);
@@ -109,7 +115,7 @@
             // 
             this.mtbAltura.Culture = new System.Globalization.CultureInfo("pt-BR");
             this.mtbAltura.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.mtbAltura.Location = new System.Drawing.Point(281, 267);
+            this.mtbAltura.Location = new System.Drawing.Point(282, 267);
             this.mtbAltura.Mask = "000";
             this.mtbAltura.Name = "mtbAltura";
             this.mtbAltura.Size = new System.Drawing.Size(100, 25);
@@ -119,16 +125,18 @@
             // lblResultado1
             // 
             this.lblResultado1.AutoSize = true;
+            this.lblResultado1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblResultado1.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.lblResultado1.Location = new System.Drawing.Point(139, 383);
             this.lblResultado1.Name = "lblResultado1";
-            this.lblResultado1.Size = new System.Drawing.Size(0, 15);
+            this.lblResultado1.Size = new System.Drawing.Size(0, 22);
             this.lblResultado1.TabIndex = 11;
             // 
             // mtbPeso
             // 
             this.mtbPeso.Culture = new System.Globalization.CultureInfo("pt-BR");
             this.mtbPeso.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.mtbPeso.Location = new System.Drawing.Point(281, 227);
+            this.mtbPeso.Location = new System.Drawing.Point(282, 227);
             this.mtbPeso.Mask = "000";
             this.mtbPeso.Name = "mtbPeso";
             this.mtbPeso.Size = new System.Drawing.Size(100, 25);
@@ -139,8 +147,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(628, 531);
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(628, 461);
             this.Controls.Add(this.mtbPeso);
             this.Controls.Add(this.lblResultado1);
             this.Controls.Add(this.mtbAltura);
